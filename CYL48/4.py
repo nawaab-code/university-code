323e41c97a6627bba7d99721a29fd9b24797a1b3
Nawaab <179938134+nawaab-code@users.noreply.github.com>
2026-06-09 17:01:00 +0530

    Update 4.py

--- CYL48/4.py
+++ CYL48/4.py
@@ -1,12 +1,6 @@
 import pandas as pd
 
-data = {
-    'Name':   ['Alice', 'Bob', None],
-    'Age':    [25, None, 28],
-    'Salary': [50000, 60000, None]
-}
-
-df = pd.DataFrame(data)
+df = pd.read_csv("file.csv")
 print(df)
 print(df.describe())
 print(df.isnull().sum())
